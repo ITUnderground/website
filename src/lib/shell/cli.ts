2c1da907917f0e8a8ebb33a8136f8278626341eb
Alexander Thoren <alct@itu.dk>
2023-03-06 23:29:50 +0100

    ✨ Added cli library

---
+++ src/lib/shell/cli.ts
@@ -0,0 +1,145 @@
+import * as prebuilt from "./commands/builtin/index"
+import * as custom from "./commands/index"
+import Dir from "./dir"
+
+type Output = {
+    user?: string,
+    server?: string,
+    cwd?: string,
+    command?: string,
+    output: string
+}
+type ParsedCommand = {
+    command: string,
+    positional: string[],
+    named: { [key: string]: string },
+    raw: string
+}
+type AccessObject = {
+    command: {
+        positional: string[],
+        named: { [key: string]: string },
+        raw: string,
+    },
+    cli: CLI,
+    dir: Dir
+}
+
+// Dir object for directory structure
+const dir = new Dir()
+
+class CLI {
+    public static commands: typeof prebuilt & typeof custom = {...prebuilt, ...custom}
+    public log: Output[] = []
+    public dir = dir
+
+    /**
+     * Extracts arguments from an itush command
+     * @param command string command to extract arguments from
+     */
+    _argParser(command: string): ParsedCommand | null {
+        /* Example: "command arg1 -a b -c d --named argument -a=1 --named2=equals -x" becomes
+         * {
+         *   command: "command",
+         *   positional: ["arg1"],
+         *   named: {
+         *     a: "b",
+         *     c: "d",
+         *     named: "argument",
+         *     a: "1",
+         *     named2: "equals",
+         *     x: ""
+         *   }
+         * }
+        */
+        const args = command.split(" ")
+        const main = args[0]
+        const named: { [key: string]: string } = {}
+        const positional: string[] = []
+        // Regex matches -a, --a, -a1, --a1, etc.
+        const namedRegex = /--?[a-zA-Z0-9]*/g
+
+        // Loop through arguments
+        for (let i = 1; i < args.length; i++) {
+            const arg = args[i]
+            if (!arg.match(namedRegex) && !args[i - 1].match(namedRegex)) {
+                positional.push(arg)
+                continue
+            }
+            // Check if using = or space
+            const [name, value] = arg.includes("=") ?
+                arg.split("=") :
+                // If we're using a space, check if the next argument is another argument or a value
+                [arg, args[i + 1] && !args[i + 1].match(namedRegex) ? 
+                    args[i + 1] : ""]
+            named[name.replace(/-+/g, "")] = value || ""
+        }
+
+        return main ? {
+            command: main,
+            positional,
+            named,
+            raw: command
+        } : null
+    }
+
+    /**
+     * Executes a ParsedCommand
+     * @param parsed ParsedCommand to execute
+     * @returns output of command
+     */
+    _execute(parsed: ParsedCommand | null): string | undefined {
+        // Check if valid command
+        if (!parsed) return ""
+        if (!(parsed.command in CLI.commands)) return `${parsed.command}: command not found`
+
+        // Run command
+        //@ts-ignore fuck you ts let me overflow
+        return CLI.commands[parsed.command as keyof typeof CLI.commands]({
+            command: {
+                positional: parsed.positional, 
+                named: parsed.named, 
+                raw: parsed.raw,
+            },
+            cli: this,
+            dir
+        })
+    }
+
+    /**
+     * Echoes a list of arguments to the console
+     * @param args list of arguments to echo
+     * @returns string of arguments joined by spaces
+     */
+    echo(...args: []) {
+        return args.join(" ")
+    }
+
+    /**
+     * Runs an itush command and adds it to the log
+     * @param command string command to run
+     * @returns output of command
+     */
+    run(command: string) {
+        // Get variables in case they are changed by command
+        const user = CLI.commands.whoami()
+        const server = CLI.commands.hostname()
+        const cwd = dir.cwd.replace("/home/itunderground", "~")
+
+        // Run command
+        const parsed = this._argParser(command)
+        const output = this._execute(parsed)
+
+        this.log.push({
+            user,
+            server,
+            cwd,
+            command,
+            output: output || "",
+        })
+        return output
+    }
+}
+
+export type { Output, ParsedCommand, AccessObject }
+export default CLI
